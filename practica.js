090479587c5ee3a3aff4d6b098865013fb1d7d2e
LIZBETH CASTILLO <lizcalixto@gmail.com>
2018-05-10 00:17:33 -0500

    ejercisios de funciones

---
+++ practica.js
@@ -0,0 +1,10 @@
+function ejemplo(){
+    alert('¿como estas Liz?');
+    alert('muy bien gracias')
+    alert('que haremos hoy')
+    alert('tratar de conquistar el mundo')
+}
+ejemplo();
+ejemplo();
+ejemplo();
+ejemplo();
---
+++ resta.js
@@ -0,0 +1,17 @@
+let nombreRestaurante={
+    nombre: "Macdonals",
+    direccion:"la primavera 250",
+    telefono:737634647949, 
+    }
+
+
+function datosCompletos(){
+    nombreRestaurante=(nombre)
+    nombreRestaurante=(direccion)
+    nombreRestaurante=(telefono)
+
+alert(datosCompletos);
+datosCompletos(nombre);
+datosCompletos(direccion);
+datosCompletos(telefono);
+}
